44c142935f4a632c359c997111d8cf0d6bc9c70d
Ольга <osloginova@yoomoney.ru>
2023-11-19 23:43:19 +0300

    First commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,29 @@
 import kotlin.random.Random
 import kotlin.random.nextInt
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.declaredFunctions
 
 interface TestRunner {
     fun <T> runTest(steps: T, test: () -> Unit)
 }
 
- open class OverrideFun : TestRunner {
+open class RunTestClass : TestRunner {
     override fun <T> runTest(steps: T, test: () -> Unit) {
+        steps!!::class.declaredFunctions.filter { it.name.startsWith("before") }.forEach {
+            invokeWrapper(steps, it)
+        }
+        test.invoke()
+        steps!!::class.declaredFunctions.filter { it.name.startsWith("after") }.forEach {
+            invokeWrapper(steps, it)
+        }
+    }
+
+    private fun <T> invokeWrapper(source: T, testFunc: KFunction<*>) {
+        println("Before invoke function")
+        testFunc.call(source)
+        println("After invoke function")
     }
 }
-
-open class NewClass : OverrideFun() {
-    fun runTest(steps: Steps, test: () -> Unit) {
-
-        val myClass = Steps()
-        val result = myClass::class.declaredFunctions.first { it.name.contains("before") }.call(myClass)
-        val result1 = myClass::class.declaredFunctions.first { it.name.contains("after") }.call(myClass)
-
-        /*val jjj = runTest(steps = myClass::class.declaredFunctions.first { it.name.contains("after") }.call(myClass)) {
-            val result = myClass::class.declaredFunctions.first { it.name.contains("before") }.call(myClass)
-            val result1 = myClass::class.declaredFunctions.first { it.name.contains("after") }.call(myClass)
-            when (a + b) {
-                4 -> println("Test success finished")
-                5 -> println("Test fail finished")
-            }
-
-        }*/
-
-        fun exampleTest() =
-            runTest(steps = result) {
-                val a = Random.nextInt(1..2)
-                val b = Random.nextInt(1..2)
-                when (a + b) {
-                    4 -> println("Test success finished")
-                    5 -> println("Test fail finished")
-                }
-
-            }
-
-    }
-}
-
 
 open class Steps {
     fun beforeTest() {
@@ -59,8 +41,7 @@ open class Steps {
 
 fun main() {
     val myClass = Steps()
-    val result = myClass::class.declaredFunctions.first { it.name.contains("before") }.call(myClass)
-    val startTest = NewClass().runTest(steps = result) {
+    RunTestClass().runTest(steps = myClass) {
         val a = Random.nextInt(1..2)
         val b = Random.nextInt(1..2)
         when (a + b) {
@@ -68,30 +49,4 @@ fun main() {
             3, 2 -> println("Test fail finished")
         }
     }
-    // val steps = object : Steps(){}
-    // val myClass = Steps()
-    //val steps = myClass::class.declaredFunctions.forEach(::println)
-    // val steps = startTest::class.declaredFunctions.forEach(::println)
-
-
-    // val myClass = MyClass()
-    //  val result = myClass::class.declaredFunctions.first { it.name.contains("before") }.call(myClass)
-    // val result1 = myClass::class.declaredFunctions.first { it.name.contains("after") }.call(myClass)
-
-
-    //@Test
-    /*  fun exampleTest() =
-          startTest.runTest(steps = myClass::class.declaredFunctions.first { it.name.contains("after") }.call(myClass)) {
-              val a = Random.nextInt(1..2)
-              val b = Random.nextInt(1..2)
-              when (a + b) {
-                  4 -> println("Test success finished")
-                  5 -> println("Test fail finished")
-              }
-
-          }
-
-      *//*println(result)
-    println(result1)*//*
-*/
 }
